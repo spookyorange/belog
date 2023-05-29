1f036f76805bf3dccd46b9163cef36dd8bbb80e2
spookyorange <korkmazcoltu@mail.ru>
2023-05-29 22:55:14 +0300

    style dashboard

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -41,44 +41,45 @@
   <div class="main-container dashboard">
     <p><a href="./create_post.php">Yeni bir gönderi oluştur</a></p>
     <h1>Hesap Yönetimi</h1>
-    <div>
-      <h2>Email değiştir</h2>
-      <form action="./update_user.php" method="POST">
-        <!-- user email can be modified -->
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" value="<?php echo $_COOKIE['user']; ?>">
-        <label for="password">Şifre</label>
-        <input type="password" name="password" id="password">
-        <label for="password_confirmation">Şifre Tekrarı</label>
-        <input type="password" name="password_confirmation" id="password_confirmation">
-        <input type="submit" value="Güncelle">
-      </form>
-    </div>
-
-    <div>
-      <h2>Varsayılan Belog Tasarım Ayarlarını değiştir</h2>
-      <form class="pref-update-form" action="update_pref.php" method="POST">
-        <label for="default_background_color_hex">Varsayılan Arkaplan Rengi(HEX kodu olarak)
-          <input type="text" name="default_background_color_hex" id="default_background_color_hex" value="<?php echo $pref_row['default_background_color_hex']; ?>">
-        </label>
-        <label for="default_foreground_color_hex">Varsayılan Yazı Rengi(HEX kodu olarak)
-          <input type="text" name="default_foreground_color_hex" id="default_foreground_color_hex" value="<?php echo $pref_row['default_foreground_color_hex']; ?>">
-        </label>
-        <label for="default_font_size_rem"> Varsayılan Yazı Boyutu (rem olarak)
-          <input type="text" name="default_font_size_rem" id="default_font_size_rem" value="<?php echo $pref_row['default_font_size_rem']; ?>">
-        </label>
-        <label for="default_font_italic">Varsayılan Yazı İtalik mi?
-          <input type="checkbox" name="default_font_italic" id="default_font_italic" <?php if ($pref_row['default_font_italic']) {
-                                                                                        echo "checked";
-                                                                                      } ?>>
-        </label>
-        <label for="default_font_bold">Varsayılan Yazı Kalın mı?
-          <input type="checkbox" name="default_font_bold" id="default_font_bold" <?php if ($pref_row['default_font_bold']) {
-                                                                                    echo "checked";
-                                                                                  } ?>>
-        </label>
-        <input type="submit" value="Güncelle">
-      </form>
+    <div class="dashboard-forms">
+      <div>
+        <h2>Email değiştir</h2>
+        <form class='standard-form' action="./update_user.php" method="POST">
+          <!-- user email can be modified -->
+          <label class='mt-1' for="email">Email
+            <input type="email" name="email" id="email" value="<?php echo $_COOKIE['user']; ?>"></label>
+          <label for="password">Şifre
+            <input type="password" name="password" id="password"></label>
+          <label for="password_confirmation">Şifre Tekrarı
+            <input type="password" name="password_confirmation" id="password_confirmation"></label>
+          <input type="submit" value="Güncelle">
+        </form>
+      </div>
+      <div>
+        <h2>Varsayılan Belog Tasarım Ayarlarını değiştir</h2>
+        <form class="standard-form pref-update-form" action="update_pref.php" method="POST">
+          <label for="default_background_color_hex">Varsayılan Arkaplan Rengi(HEX kodu olarak)
+            <input type="text" name="default_background_color_hex" id="default_background_color_hex" value="<?php echo $pref_row['default_background_color_hex']; ?>">
+          </label>
+          <label for="default_foreground_color_hex">Varsayılan Yazı Rengi(HEX kodu olarak)
+            <input type="text" name="default_foreground_color_hex" id="default_foreground_color_hex" value="<?php echo $pref_row['default_foreground_color_hex']; ?>">
+          </label>
+          <label for="default_font_size_rem"> Varsayılan Yazı Boyutu (rem olarak)
+            <input type="text" name="default_font_size_rem" id="default_font_size_rem" value="<?php echo $pref_row['default_font_size_rem']; ?>">
+          </label>
+          <label class="chbox-in-db" for="default_font_italic">Varsayılan Yazı İtalik mi?
+            <input type="checkbox" name="default_font_italic" id="default_font_italic" <?php if ($pref_row['default_font_italic']) {
+                                                                                          echo "checked";
+                                                                                        } ?>>
+          </label>
+          <label class="chbox-in-db" for="default_font_bold">Varsayılan Yazı Kalın mı?
+            <input type="checkbox" name="default_font_bold" id="default_font_bold" <?php if ($pref_row['default_font_bold']) {
+                                                                                      echo "checked";
+                                                                                    } ?>>
+          </label>
+          <input type="submit" value="Güncelle">
+        </form>
+      </div>
     </div>
   </div>
   <?php include './footer.php'; ?>
--- sign_up.php
+++ sign_up.php
@@ -12,18 +12,18 @@
 <body>
   <?php include('header.php'); ?>
   <div class="main-container">
-  <div class="login-exclusive">
-    <h1>Hesap Oluştur</h1>
-    <form class="sign-up-form standard-form" action="create_the_account.php" method="POST">
-      <label for="email">E-posta
-      <input type="text" name="email" id="email"></label>
-      <label for="password">Şifre
-      <input type="password" name="password" id="password"></label>
-      <input type="submit" value="Kayıt Ol">
-    </form>
-  <a class="secondary-button" href="sign_in.php">Giriş Yap</a>
+    <div class="login-exclusive">
+      <h1>Hesap Oluştur</h1>
+      <form class="sign-up-form standard-form" action="create_the_account.php" method="POST">
+        <label for="email">E-posta
+          <input type="text" name="email" id="email"></label>
+        <label for="password">Şifre
+          <input type="password" name="password" id="password"></label>
+        <input type="submit" value="Kayıt Ol">
+      </form>
+      <a class="secondary-button" href="sign_in.php">Giriş Yap</a>
 
-  </div>
+    </div>
   </div>
   <?php include './footer.php'; ?>
 </body>
